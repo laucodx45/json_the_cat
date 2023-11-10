e3f3a0d07f8d3dddadada564f6c716725abf6f5c
Cody <codylau6233@hotmail.com>
2023-11-09 23:20:50 -0800

    This script uses the Cat API to fetch information about a specified cat breed and display its description. Add error handling.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,23 @@
 const request = require('request');
 
-const url = 'https://api.thecatapi.com/v1/breeds/search'
+const breed = process.argv.slice(2);
+
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 request(url, function(error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  
+  if (error) {
+    console.error('error: invalid breedName', error);
+    process.exit(1);
+  }
+
+  const data = JSON.parse(body);
+
+  if (data.length === 0) {
+    console.error('Invalid breedName');
+    process.exit(1);
+  }
+
+  console.log(data[0].description);
+  
 });
